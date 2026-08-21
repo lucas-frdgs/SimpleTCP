f36634615ace1e18ca0de309a956f95696f613c7
Lucas Rodrigues <117823586+lucas-frdgs@users.noreply.github.com>
2026-08-20 22:38:34 -0300

    Add connection state regression tests

--- SimpleTCP.Tests/CommTest.cs
+++ SimpleTCP.Tests/CommTest.cs
@@ -68,8 +68,99 @@ namespace SimpleTCP.Tests
             }
 
             Assert.IsTrue(true);
+        }
 
-            
+        [TestMethod]
+        public void ClientConnectionEventsAreRaisedOncePerTransition()
+        {
+            var listener = new System.Net.Sockets.TcpListener(System.Net.IPAddress.Loopback, 0);
+            listener.Start();
+
+            var endpoint = (System.Net.IPEndPoint)listener.LocalEndpoint;
+            var client = new SimpleTcpClient();
+            System.Net.Sockets.TcpClient serverSideClient = null;
+            int started = 0;
+            int interrupted = 0;
+            int closed = 0;
+
+            client.ConnectionStarted += (sender, args) => System.Threading.Interlocked.Increment(ref started);
+            client.ConnectionInterrupted += (sender, args) => System.Threading.Interlocked.Increment(ref interrupted);
+            client.ConnectionClosed += (sender, args) => System.Threading.Interlocked.Increment(ref closed);
+
+            try
+            {
+                client.Connect(System.Net.IPAddress.Loopback.ToString(), endpoint.Port);
+                serverSideClient = listener.AcceptTcpClient();
+
+                Assert.IsTrue(client.IsConnected);
+                Assert.AreEqual(1, started);
+
+                client.Disconnect();
+                System.Threading.Thread.Sleep(100);
+
+                Assert.IsFalse(client.IsConnected);
+                Assert.AreEqual(1, started);
+                Assert.AreEqual(0, interrupted);
+                Assert.AreEqual(1, closed);
+
+                client.Disconnect();
+                System.Threading.Thread.Sleep(50);
+
+                Assert.AreEqual(0, interrupted);
+                Assert.AreEqual(1, closed);
+            }
+            finally
+            {
+                if (serverSideClient != null) { serverSideClient.Close(); }
+                client.Dispose();
+                listener.Stop();
+            }
+        }
+
+        [TestMethod]
+        public void RemoteDisconnectRaisesConnectionInterruptedOnce()
+        {
+            var listener = new System.Net.Sockets.TcpListener(System.Net.IPAddress.Loopback, 0);
+            listener.Start();
+
+            var endpoint = (System.Net.IPEndPoint)listener.LocalEndpoint;
+            var client = new SimpleTcpClient();
+            System.Net.Sockets.TcpClient serverSideClient = null;
+            int started = 0;
+            int interrupted = 0;
+            int closed = 0;
+
+            client.ConnectionStarted += (sender, args) => System.Threading.Interlocked.Increment(ref started);
+            client.ConnectionInterrupted += (sender, args) => System.Threading.Interlocked.Increment(ref interrupted);
+            client.ConnectionClosed += (sender, args) => System.Threading.Interlocked.Increment(ref closed);
+
+            try
+            {
+                client.Connect(System.Net.IPAddress.Loopback.ToString(), endpoint.Port);
+                serverSideClient = listener.AcceptTcpClient();
+
+                Assert.AreEqual(1, started);
+                Assert.IsTrue(client.IsConnected);
+
+                serverSideClient.Close();
+                serverSideClient = null;
+
+                bool interruptedDetected = System.Threading.SpinWait.SpinUntil(() => interrupted == 1, 2000);
+                Assert.IsTrue(interruptedDetected, "Client did not detect the remote disconnect.");
+
+                System.Threading.Thread.Sleep(100);
+
+                Assert.IsFalse(client.IsConnected);
+                Assert.AreEqual(1, started);
+                Assert.AreEqual(1, interrupted);
+                Assert.AreEqual(0, closed);
+            }
+            finally
+            {
+                if (serverSideClient != null) { serverSideClient.Close(); }
+                client.Dispose();
+                listener.Stop();
+            }
         }
     }
 }
